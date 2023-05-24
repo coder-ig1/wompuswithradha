d2792264aea1eefc16616d616b8fa97b9be1f370
Ezra Newman <91305744+coder-ig1@users.noreply.github.com>
2023-05-24 12:26:25 -0400

    not qworking rn lmao

--- Cave.java
+++ Cave.java
@@ -1,3 +1,9 @@
 public class Cave {
-    
+    Cave[] linkedCaves = new Cave[3];
+    CaveSystem caveSystem;
+    public Cave(int caveNumber, int caveNumber2, int caveNumber3, CaveSystem caveSystem) {
+        linkedCaves[0] = caveSystem.accsesCave(caveNumber);
+        linkedCaves[1] = caveSystem.accsesCave(caveNumber2);
+        linkedCaves[2] = caveSystem.accsesCave(caveNumber3);
+    }
 }
--- CaveSystem.java
+++ CaveSystem.java
@@ -1,3 +1,9 @@
 public class CaveSystem {
-    
+    Cave[] caves = new Cave[20];
+    public CaveSystem() {
+        
+    }
+    public Cave accsesCave(int caveNumber) {
+        return caves[caveNumber];
+    }
 }
