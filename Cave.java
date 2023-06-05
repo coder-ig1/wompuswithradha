ee25e569df1c3367f3293a385ce1a10c992b16f2
Ezra Newman <91305744+coder-ig1@users.noreply.github.com>
2023-06-05 12:34:18 -0400

    hahah commit

--- Cave.java
+++ Cave.java
@@ -5,5 +5,20 @@ public class Cave {
         linkedCaves[0] = caveSystem.accsesCave(caveNumber);
         linkedCaves[1] = caveSystem.accsesCave(caveNumber2);
         linkedCaves[2] = caveSystem.accsesCave(caveNumber3);
+        this.caveSystem = caveSystem;
+    }
+    public Cave(){
+        linkedCaves[0] = null;
+        linkedCaves[1] = null;
+        linkedCaves[2] = null;
+        this.caveSystem = null;
+    }
+    public void setLinkedCaves(int caveNumber, int caveNumber2, int caveNumber3) {
+        linkedCaves[0] = caveSystem.accsesCave(caveNumber);
+        linkedCaves[1] = caveSystem.accsesCave(caveNumber2);
+        linkedCaves[2] = caveSystem.accsesCave(caveNumber3);
+    }
+    public void setCaveSystem(CaveSystem caveSystem) {
+        this.caveSystem = caveSystem;
     }
 }
