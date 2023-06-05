cba619123b5be60fae07f3ce5c28d3244dc181c3
Ezra Newman <91305744+coder-ig1@users.noreply.github.com>
2023-06-05 17:31:19 -0400

    added some loop to main

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Main
 {
   private static boolean gameOn = true;
   public static void main(String[] args)
   {
+    CaveSystem cavesystem = new CaveSystem()
+    Player player = new Player(cavesystem,cavesystem.accessCave(1));
     //put code here
     while(gameOn){
-      
     }
     
+  }
+  private void promptForMove(){
+    Scanner takeInput = new Scanner(System.in);
+    System.out.println("you are in room ")
   }
 }
