446d736e82cc1fc1ec99411c69ac17be34dae802
Ezra Newman <91305744+coder-ig1@users.noreply.github.com>
2023-06-05 22:51:38 -0400

    Different eroor

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main
   private static boolean gameOn = true;
   private static CaveSystem cavesystem = new CaveSystem();
   private static Player player = new Player(cavesystem,findEmptyCave(cavesystem));
+  private static Scanner takeInput = new Scanner(System.in);
   public static void main(String[] args)
   {
     //put code here
@@ -12,36 +13,38 @@ public class Main
       String resultOfPrompt = promptForMove();
       doMove(resultOfPrompt);
     }
+    takeInput.close();
     
   }
   private static void doMove(String resultOfPrompt){
-    Scanner takeInput = new Scanner(System.in);
+    
     if(resultOfPrompt.equals("1")){
       System.out.println("Which room would you like to move to?");
       String input2 = takeInput.nextLine();
       int roomNum = Integer.parseInt(input2);
+      takeInput.close();
       player.move(roomNum);
     }
     else if(resultOfPrompt.equals("2")){
       System.out.println("Which room would you like to shoot into?");
       String input2 = takeInput.nextLine();
       int roomNum = Integer.parseInt(input2);
+      takeInput.close();
       player.shootArrow(roomNum);
     }
     else{
       System.out.println("Invalid input");
+      takeInput.close();
     }
-    takeInput.close();
+    
   }
   private static String promptForMove(){
-    Scanner takeInput = new Scanner(System.in);
     System.out.println("you are in cave");
     player.printRoomInfo();
     System.out.println("What would you like to do?");
     System.out.print("1. Move,");
     System.out.println("2. Shoot");
     String input = takeInput.nextLine();
-    takeInput.close();
     return input;
   }
   
