29eeb87d4ace898a4d70c231124fc52f235e8f1c
Radha <67490858+rsm-13@users.noreply.github.com>
2023-06-06 16:19:29 -0400

    instructions

--- Main.java
+++ Main.java
@@ -13,24 +13,25 @@ public class Main
   private static Scanner takeInput = new Scanner(System.in);  // create scanner object for taking task and cave input from user
   public static void main(String[] args)
   {
+    // instructions
+    System.out.println();
+    System.out.println("Welcome to Hunt the Wumpus!");
+    System.out.println();
+    System.out.println("You are in a cave system with 20 caves. Each cave is linked to three other caves. In one of the caves is the Wumpus; it is your mission to kill it.");
+    System.out.println("Two of the caves contain bottomless pits, and will plummett to your death upon entrance into their caves.");
+    System.out.println("Two of the caves contain bats, which will pick you up and drop you in a random cave if you enter them.");
+    System.out.println("You have 5 arrows on your quiver. Once you run out, if the Wumpus is not dead, the game is over.");
+    System.out.println("You have two choices of action: 1) move caves to one of the three adjacent ones or 2) shoot an arrow at an adjacent cave.");
+    System.out.println("If your arrow strikes the Wumpus, it dies and you win the game.");
+    System.out.println("If the arrow misses the Wumpus, the Wumpus will be startled and move to another room.");
+    System.out.println("If you strike a bat, the bat will die.");
+    System.out.println("Upon entering a room, if the Wumpus is nearby, you will smell a disgusting odor.");
+    System.out.println("If a bat(s) is/are nearby, you will hear a rustling.");
+    System.out.println("If you are nearby a pit, you will feel a cold wind blowing.");
+    System.out.println("It's your job to kill the Wumpus before you run out of arrows and make it out alive. Good luck!");
+
     // shall we play a game...
     while (gameOn) {
-
-      // instructions
-      System.out.println("Welcome to Hunt the Wumpus!");
-      System.out.println("You are in a cave system with 20 caves. Each cave is linked to three other caves. In one of the caves is the Wumpus; it is your mission to kill it.");
-      System.out.println("Two of the caves contain bottomless pits, and will plummett to your death upon entrance into their caves.");
-      System.out.println("Two of the caves contain bats, which will pick you up and drop you in a random cave if you enter them.");
-      System.out.println("You have 5 arrows on your quiver. Once you run out, if the Wumpus is not dead, the game is over.");
-      System.out.println("You have two choices of action: 1) move caves to one of the three adjacent ones or 2) shoot an arrow at an adjacent cave.");
-      System.out.println("If your arrow strikes the Wumpus, it dies and you win the game.");
-      System.out.println("If the arrow misses the Wumpus, the Wumpus will be startled and move to another room.");
-      System.out.println("If you strike a bat, the bat will die.");
-      System.out.println("Upon entering a room, if the Wumpus is nearby, you will smell a disgusting odor.");
-      System.out.println("If a bat(s) is/are nearby, you will hear a rustling.");
-      System.out.println("If you are nearby a pit, you will feel a cold wind blowing.");
-      System.out.println("It's your job to kill the Wumpus before you run out of arrows and make it out alive. Good luck!");
-      
       // prompt user for action and do that action while the game isn't over
       String resultOfPrompt = promptForMove();
       doMove(resultOfPrompt);
@@ -40,7 +41,8 @@ public class Main
   // ask user what they would like to do
   private static String promptForMove(){
     // Give information
-    System.out.println("you are in cave "+String.valueOf(player.getCurrentCave().getCaveNum()));
+    System.out.println(); // line break
+    System.out.println("You are in cave " + String.valueOf(player.getCurrentCave().getCaveNum()) + ".");
     player.printRoomInfo();
 
     // prompt for move or shoot
